4a5a79f44d5ae3aec7a632e5e5cf4110622d4ab1
gmahone <gmahone@gmail.com>
2022-05-30 22:15:46 +0200

    add two arrow function versions

--- convert_string_to_number.ts
+++ convert_string_to_number.ts
@@ -1,3 +1,11 @@
 export function stringToNumber(str: string): number {
   return Number(str);
 }
+
+
+// arrow function version
+export const stringToNumber = (str: string): number => Number(str);
+
+
+// other solutions
+export const stringToNumber = (str: string): number => +str;
