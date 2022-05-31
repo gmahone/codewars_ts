621c7f625d59ff16a6fe1a5ea7eebd6ddff64f03
gmahone <gmahone@gmail.com>
2022-05-31 21:49:03 +0200

    add solution without template literal

--- a_needle_in_the_haystack.js
+++ a_needle_in_the_haystack.js
@@ -2,3 +2,10 @@ export function findNeedle(haystack: any[]):string {
   let haystackIndex = haystack.indexOf("needle");
   return `found the needle at position ${haystackIndex}`;
 }
+
+// other solutions
+
+// one line without template literal
+export function findNeedle(haystack: any[]):string {
+  return "found the needle at position " + haystack.indexOf('needle');
+}
