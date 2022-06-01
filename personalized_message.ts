bfc76170fa5c477cda74442076b86e13d60a2795
gmahone <gmahone@gmail.com>
2022-06-01 20:56:12 +0200

    add template literal solution

--- personalized_message.ts
+++ personalized_message.ts
@@ -1,3 +1,11 @@
 export function greet(name: string, owner: string): string{
   return "Hello " + (name === owner ? "boss" : "guest");
 }
+
+
+// other solutions
+
+// template literal solution
+export function greet(name: string, owner: string): string{
+  return `Hello ${name === owner ? 'boss': 'guest'}`;
+}
