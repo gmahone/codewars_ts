f3431b5783d9b6051a4e015f284c7993fe16e4e6
gmahone <gmahone@gmail.com>
2022-06-04 22:30:32 +0200

    add initial solution

--- smallest_integer_in_array.ts
+++ smallest_integer_in_array.ts
@@ -1,3 +1,9 @@
+/**
 export function findSmallestInt(args: number[]): number {
   throw new Error("This method or operation is not implemented.");
+}
+**/
+
+export function findSmallestInt(args: number[]): number {
+  return Math.min(args);
 }
