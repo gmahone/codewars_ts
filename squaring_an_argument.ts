5062e0ce353b9a4e695d3fc289c5a1bb1bdeec7b
gmahone <gmahone@gmail.com>
2022-06-04 22:22:47 +0200

    add standard solution version

--- squaring_an_argument.ts
+++ squaring_an_argument.ts
@@ -4,3 +4,8 @@ export const square = (num: number): number => num * num;
 
 // solution with input type any
 export const square = (n : any ) : any => n ** 2
+
+// add standard function version
+export function square(n: number): number{
+  return n * n
+}
