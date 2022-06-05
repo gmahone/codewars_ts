7d8adbb759ff029a847fabff07bdfe3c37d15a2d
gmahone <gmahone@gmail.com>
2022-06-05 22:10:28 +0200

    fixed bug in solution

--- character_recognition_software.ts
+++ character_recognition_software.ts
@@ -1,6 +1,6 @@
 export function correct(s: string): string{
-  s.replace(/5/g, "S");
-  s.replace(/0/g, "O");
-  s.replace(/1/g, "I");
+  s = s.replace(/5/g, "S");
+  s = s.replace(/0/g, "O");
+  s = s.replace(/1/g, "I");
   return s;
 }
