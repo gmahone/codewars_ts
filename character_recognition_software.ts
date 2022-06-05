da5428541b4fa94fbeca0f9e1a4c845b99737650
gmahone <gmahone@gmail.com>
2022-06-05 22:15:10 +0200

    added solution using object mapping

--- character_recognition_software.ts
+++ character_recognition_software.ts
@@ -14,3 +14,17 @@ export function correct(s: string): string{
 
 // arrow function version
 export const correct = (s: string) => s.replace(/1/g, 'I').replace(/5/g, 'S').replace(/0/g, 'O');
+
+
+// using an object mapping
+export function correct(s: string): string{
+  const mistakes: {[key: string]: string} = {
+    '5': 'S',
+    '0': 'O',
+    '1': 'I',
+  };
+  return [...s].map(ch => {
+    if (/\d/.test(ch)) return mistakes[ch];
+    return ch;
+  }).join('');
+}
