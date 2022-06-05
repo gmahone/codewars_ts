32ff035b3fe27304cbe9888b26b9b4ee0a7a0c29
gmahone <gmahone@gmail.com>
2022-06-05 22:11:57 +0200

    add simpler solution

--- character_recognition_software.ts
+++ character_recognition_software.ts
@@ -4,3 +4,10 @@ export function correct(s: string): string{
   s = s.replace(/1/g, "I");
   return s;
 }
+
+// other solutions
+
+// simpler solution
+export function correct(s: string): string{
+  return s.replace(/1/g, 'I').replace(/5/g, 'S').replace(/0/g, 'O')
+}
