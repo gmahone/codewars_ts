110c3570fd061dfa5d3c89556c5c56202f8f09e2
gmahone <gmahone@gmail.com>
2022-06-06 16:33:48 +0200

    add arrow function solution version

--- hex_to_decimal.ts
+++ hex_to_decimal.ts
@@ -1,3 +1,6 @@
 export function hexToDec(hexString:string):number{
   return parseInt(hexString, 16);
 }
+
+// arrow function version
+export const hexToDec = (hexString:string):number => parseInt(hexString, 16);
