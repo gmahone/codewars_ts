27b335260570b275c231b9cf90e7c1a4e54f5363
gmahone <gmahone@gmail.com>
2022-06-07 21:38:06 +0200

    added arrow function solution with ternary multiplication

--- simple_multiplication.ts
+++ simple_multiplication.ts
@@ -1,3 +1,9 @@
 export function simpleMultiplication(num: number): number{
   return num & 1 ? num * 9 : num * 8;
 }
+
+// other solutions
+
+// arrow function with ternary multiplication
+export const simpleMultiplication = (num: number): number =>
+  num * (num % 2 === 0 ? 8 : 9);
