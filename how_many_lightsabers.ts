42d4402979bafd71e64e1838b78c4e0f24aabf14
gmahone <gmahone@gmail.com>
2022-06-09 22:15:00 +0200

    add arrow function version

--- how_many_lightsabers.ts
+++ how_many_lightsabers.ts
@@ -2,3 +2,7 @@ export function howManyLightsabersDoYouOwn(name?: any): number {
     return name === "Zach" ? 18 : 0;
 }
 
+// other solutions
+
+// arrow function version
+export const howManyLightsabersDoYouOwn = (name?: any): number => name === 'Zach' ? 18 : 0;
