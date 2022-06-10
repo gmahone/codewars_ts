a6d4f530854bba6c2456e5e5c6aaf2a7e31a03b1
gmahone <gmahone@gmail.com>
2022-06-10 22:48:38 +0200

    added clean double map solution

--- maximum_length_distance.ts
+++ maximum_length_distance.ts
@@ -30,3 +30,13 @@ export class G964 {
         return max;
     }
 }
+
+// cleaner double math solution
+export class G964 {
+  public static mxdiflg = (a1, a2) => {
+    if (!a1.length || !a2.length) return -1;
+    const x = Math.max(...a1.map(v => v.length)) - Math.min(...a2.map(v => v.length));
+    const y = Math.max(...a2.map(v => v.length)) - Math.min(...a1.map(v => v.length));
+    return x > y ? x : y;
+  }
+}
