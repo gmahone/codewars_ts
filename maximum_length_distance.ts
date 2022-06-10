dfee68bad4587b8cbf5204d689536afa7c95c9ab
gmahone <gmahone@gmail.com>
2022-06-10 22:45:45 +0200

    bugfixing initial solution

--- maximum_length_distance.ts
+++ maximum_length_distance.ts
@@ -8,7 +8,7 @@ export class G964 {
       let a1Maxa2Min = Math.max(...a1Lengths) - Math.min(...a2Lengths);
       let a1Mina2Max = Math.abs(Math.max(...a2Lengths) - Math.min(...a1Lengths));
       let maxDiff = Math.max(a1Maxa2Min, a1Mina2Max);
-      if(maxDiff){
+      if(a1.length && a2.length){
         result = maxDiff;
       }
       return result;
