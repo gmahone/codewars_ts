576e429c91f82ee78a3b91adb671253b9f4e47d0
gmahone <gmahone@gmail.com>
2022-06-17 22:12:58 +0200

    add arrow function version

--- remove_string_spaces.ts
+++ remove_string_spaces.ts
@@ -2,8 +2,11 @@ export function noSpace(x:string):string {
   return x.split(" ").join("");
 }
 
+// arrow function version
+export const noSpace = (x: string):string => x.split(' ').join('');
+
 
 // add regex solution
 export function noSpace(x:string):string {
-  return x.replace(/\s/g, '');
+  return x.replace(/\s/g, "");
 }
