11cab6c84787c1178b1a266b2563b59a59d68f8b
gmahone <gmahone@gmail.com>
2022-06-17 22:11:39 +0200

    add regex solution

--- remove_string_spaces.ts
+++ remove_string_spaces.ts
@@ -1,3 +1,9 @@
 export function noSpace(x:string):string {
   return x.split(" ").join("");
 }
+
+
+// add regex solution
+export function noSpace(x:string):string {
+  return x.replace(/\s/g, '');
+}
