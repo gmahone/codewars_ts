486ae2de81c997dd9ee2ca66edb6c73b17f7c231
gmahone <gmahone@gmail.com>
2022-06-18 23:01:21 +0200

    working on initial solution

--- sum_of_triangular_numbers.ts
+++ sum_of_triangular_numbers.ts
@@ -1,7 +1,9 @@
 export function sumTriangularNumbers(n:number):number {
-  let result = 0;
+  var result:number = 0;
+  var triArray:number[] = [0];
   for(let i = 1; i < (n + 1); i++){
     result += i;
+    triArray[i-1] = result;
   }
-  return result;
+  return triArray.reduce((acc,c) => acc + c)
 }
