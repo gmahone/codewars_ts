a55ebcd388cb01e5226a0338f470b7d269fb6c1c
gmahone <gmahone@gmail.com>
2022-06-19 22:31:05 +0200

    add single line solution

--- usd_to_cny.ts
+++ usd_to_cny.ts
@@ -2,3 +2,8 @@ export function usdcny(usd: number): string {
   let convertedValue = (usd * 6.75).toFixed(2)
   return `${convertedValue} Chinese Yuan`;
 }
+
+// one line
+export function usdcny(usd: number): string {
+  return `${(usd * 6.75).toFixed(2)} Chinese Yuan`
+}
