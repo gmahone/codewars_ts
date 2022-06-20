bbc16a7ce595343972ce428cf59ae3ef2a0cb535
gmahone <gmahone@gmail.com>
2022-06-20 20:33:47 +0200

    add arrow function version

--- javascript_array_filter.ts
+++ javascript_array_filter.ts
@@ -6,3 +6,6 @@ export const getEvenNumbers = (numbersArray : number[]) : number[] => {
 export const getEvenNumbers = (numbersArray : number[]) : number[] => {
   return numbersArray.filter(a => a % 2 === 0)
 }
+
+// arrow function version
+export  const getEvenNumbers = (numbersArray : number[]) : number[] => numbersArray.filter(a => a % 2 === 0) 
