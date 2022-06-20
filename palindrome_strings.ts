4d9e1a943805509505eeed0ed31c74191e4bd2e3
gmahone <gmahone@gmail.com>
2022-06-20 20:37:26 +0200

    add arrow function version

--- palindrome_strings.ts
+++ palindrome_strings.ts
@@ -1,3 +1,7 @@
 export function isPalindrome(line: string): boolean {
   return line === line.split("").reverse().join("")
 }
+
+
+// arrow function version
+export const isPalindrome = (line: string): boolean => line === line.split("").reverse().join("");
