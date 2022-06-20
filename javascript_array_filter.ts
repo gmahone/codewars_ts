46dd58bf7135241a07014c1bdebd720c33cc2695
gmahone <gmahone@gmail.com>
2022-06-20 20:33:08 +0200

    add alternative solution

--- javascript_array_filter.ts
+++ javascript_array_filter.ts
@@ -1,3 +1,8 @@
 export const getEvenNumbers = (numbersArray : number[]) : number[] => {
   return numbersArray.filter(a => !(a & 1))
 }
+
+// alternative
+export const getEvenNumbers = (numbersArray : number[]) : number[] => {
+  return numbersArray.filter(a => a % 2 === 0)
+}
