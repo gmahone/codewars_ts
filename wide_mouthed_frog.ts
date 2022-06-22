c6418ef55bfa16dafd484bdc13e1c999ffdc4d8a
gmahone <gmahone@gmail.com>
2022-06-22 22:01:41 +0200

    add arrow function solution version

--- wide_mouthed_frog.ts
+++ wide_mouthed_frog.ts
@@ -1,3 +1,7 @@
 export function mouthSize(animal: string): string {
   return animal.toLowerCase() === "alligator" ? "small" : "wide";
 }
+
+
+// arrow function version
+export const mouthSize = (animal: string): string => animal.toLowerCase() === "alligator" ? "small" : "wide";
