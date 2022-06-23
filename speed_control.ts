642c47fb110a7a237f26803af2e8f70aef638204
gmahone <gmahone@gmail.com>
2022-06-23 15:57:39 +0200

    add initial solution

--- speed_control.ts
+++ speed_control.ts
@@ -1,6 +1,10 @@
 export class G964 {
 
     public static gps = (s, x) => {
-        // your code
+        speedArray = [];
+        for(let i = 1; i < x.length; i++){
+            speedArray.push(3600 * (x[i] - x[i-1]) / s);
+        }
+        return speedArray;
     }  
 }
