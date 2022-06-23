314ba6020f79b1004c9bd33a78785f212c0b18a6
gmahone <gmahone@gmail.com>
2022-06-23 16:06:22 +0200

    add length < 2 case return to solution

--- speed_control.ts
+++ speed_control.ts
@@ -1,12 +1,15 @@
 export class G964 {
 
     public static gps = (s, x) => {
-        let speedArray = [];
-        let currSpeed;
-        for(let i = 1; i < x.length; i++){
-            currSpeed = 3600 * (x[i] - x[i-1]) / s;
-            speedArray.push(Math.floor(currSpeed));
-        }
-        return Math.max(...speedArray);
+      let speedArray = [];
+      let currSpeed;
+      if(x.length < 2){
+        return 0;
+      }
+      for(let i = 1; i < x.length; i++){
+          currSpeed = 3600 * (x[i] - x[i-1]) / s;
+          speedArray.push(Math.floor(currSpeed));
+      }
+      return Math.max(...speedArray);
     }  
 }
