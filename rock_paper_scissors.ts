faf0ee57165f470a7ae1b5c5d43850631d98ca10
gmahone <gmahone@gmail.com>
2022-06-24 21:43:31 +0200

    feat: add initial solution

--- rock_paper_scissors.ts
+++ rock_paper_scissors.ts
@@ -1,3 +1,29 @@
 export function rps(p1: string, p2: string): string{
-  return ""
+  let winner: string;
+  if(p1 === p2){
+    return("Draw");
+  }
+  if(p1 === "rock"){
+    switch(p2){
+      case "paper":
+        winner = "2";
+      case "scissors":
+        winner = "1";
+    }
+  } else if(p1 === "paper"){
+    switch(p2){
+      case "rock":
+        winner = "1";
+      case "scissors":
+        winner = "2";
+    }
+  } else if(p1 === "scissors"){
+    switch(p2){
+      case "rock":
+        winner = "2";
+      case "paper":
+        winner = "1";
+    }
+  }
+  return `Player ${winner} won!`;
 }
