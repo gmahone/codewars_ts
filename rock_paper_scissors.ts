87dc2fd6e1c2ba4c7aebe8902f12db256b890e84
gmahone <gmahone@gmail.com>
2022-06-24 21:55:18 +0200

    feat: add solution that tests for p1 win directly

--- rock_paper_scissors.ts
+++ rock_paper_scissors.ts
@@ -33,3 +33,14 @@ export function rps(p1: string, p2: string): string{
   }
   return `Player ${winner} won!`;
 }
+
+// more explicit solution rather than endless tests
+export function rps(p1: string, p2: string): string{
+  if (p1 === p2) {
+    return 'Draw!'
+  } else if (p1 === 'scissors' && p2 === 'paper' || p1 === 'rock' && p2 === 'scissors' || p1 === 'paper' && p2 === 'rock') {
+    return 'Player 1 won!'
+  } else {
+    return 'Player 2 won!'
+  } 
+}
