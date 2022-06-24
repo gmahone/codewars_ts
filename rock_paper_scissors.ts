98dc6fa634d61089d87fb06d1d809e76092ed143
gmahone <gmahone@gmail.com>
2022-06-24 21:49:40 +0200

    fix: working on initial solution

--- rock_paper_scissors.ts
+++ rock_paper_scissors.ts
@@ -1,28 +1,34 @@
 export function rps(p1: string, p2: string): string{
-  let winner: string;
+  let winner;
   if(p1 === p2){
-    return("Draw");
+    return("Draw!");
   }
   if(p1 === "rock"){
     switch(p2){
       case "paper":
         winner = "2";
+        break;
       case "scissors":
         winner = "1";
+        break;
     }
   } else if(p1 === "paper"){
     switch(p2){
       case "rock":
         winner = "1";
+        break;
       case "scissors":
         winner = "2";
+        break;
     }
   } else if(p1 === "scissors"){
     switch(p2){
       case "rock":
         winner = "2";
+        break;
       case "paper":
         winner = "1";
+        break;
     }
   }
   return `Player ${winner} won!`;
