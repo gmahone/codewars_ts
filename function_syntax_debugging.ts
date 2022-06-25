a7d4cceea6d872791250b4f2c72a686f512e0f5d
gmahone <gmahone@gmail.com>
2022-06-25 09:16:12 +0200

    feat: add arrow function solution version

--- function_syntax_debugging.ts
+++ function_syntax_debugging.ts
@@ -1,3 +1,6 @@
 export function main(verb: string, noun: string): string {
   return(verb + noun);
 }
+
+// arrow function version
+export const main = (verb: string, noun: string): string => verb + noun
