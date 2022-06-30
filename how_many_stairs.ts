68418826d0a01b5f99545bda0863a798e8584702
gmahone <gmahone@gmail.com>
2022-06-30 16:59:41 +0200

    feat: add arrow function version

--- how_many_stairs.ts
+++ how_many_stairs.ts
@@ -2,3 +2,7 @@ export function stairsIn20(stairs:number[][]) {
   let result = stairs.flat().reduce((acc,c) => acc+c, 0) * 20;
   return result;
 }
+
+
+// arrow function version
+export const stairsIn20 = (stairs:number[][]): number => stairs.flat().reduce((acc,c) => acc+c, 0) * 20;
