5b8af3ed5b6d1b400a4834f92f5d885aa1537eb7
gmahone <gmahone@gmail.com>
2022-06-30 21:40:34 +0200

    feat: add arrow function version

--- remove_exclamation_except_end.ts
+++ remove_exclamation_except_end.ts
@@ -1,3 +1,5 @@
 export function remove(s: string): string {
-  return s.split("!").join("") + "!"
+  return s.split("!").join("") + "!";
 }
+
+export const remove = (s: string): string => s.split("!").join("") + "!";
