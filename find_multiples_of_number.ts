84b65bb535b5c1c711a600f267d3d08bbb15f44e
gmahone <gmahone@gmail.com>
2022-07-01 17:20:47 +0200

    fix: replace int with Math.floor

--- find_multiples_of_number.ts
+++ find_multiples_of_number.ts
@@ -1,5 +1,5 @@
 export function findMultiples(integer: number, limit: number): number[] {
-  let loopLimit = int(limit / integer);
+  let loopLimit = Math.floor(limit / integer);
   let result = new Array(loopLimit).fill(integer);
   return result.map((e,i) => e * (i+1))
 }
