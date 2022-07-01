5700c358b7ea55b827685eba6a7158f19afd7214
gmahone <gmahone@gmail.com>
2022-07-01 17:19:42 +0200

    feat: working on initial solution

--- find_multiples_of_number.ts
+++ find_multiples_of_number.ts
@@ -1,3 +1,5 @@
 export function findMultiples(integer: number, limit: number): number[] {
-  //your code here
+  let loopLimit = int(limit / integer);
+  let result = new Array(loopLimit).fill(integer);
+  return result.map((e,i) => e * (i+1))
 }
