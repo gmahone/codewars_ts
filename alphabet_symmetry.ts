9ee159bd964e993d1edc0bf09566263303b82216
gmahone <gmahone@gmail.com>
2022-07-01 22:37:04 +0200

    feat: add initial solution

--- alphabet_symmetry.ts
+++ alphabet_symmetry.ts
@@ -1,3 +1,8 @@
-export function solve(arr: string[]) {
-  // your code here
+export function comparePosition(str: string): number {
+  return str.toLowerCase().split("").map((e,i) => charCodeAt(e) - 97 === i ? 1 : 0).reduce((acc,c) => acc + c, 0);
+}
+
+
+export function solve(arr: string[]): number[] {
+  return arr.map(a => comparePosition(a));
 }
