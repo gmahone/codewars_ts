ab27a82bf6bd3017128e346cdc2cdd12eeae2256
gmahone <gmahone@gmail.com>
2022-07-04 17:36:51 +0200

    feat: add initial solution

--- all_inclusive.ts
+++ all_inclusive.ts
@@ -1,6 +1,10 @@
 export function containAllRots(str: string, arr: string[]): boolean {
-  let result: boolean = False;
+  let result: boolean = True;
   for(let i: number = 0; i < arr.length; i++){
-    
+    if(!arr.includes(str)){
+      return False;
+    }
+    str = str.substring(1) + str.substring(0,1);
   }
+  return result;
 }
