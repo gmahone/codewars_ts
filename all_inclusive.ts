a2e0792e9ead844aa451774141674a128aa92766
gmahone <gmahone@gmail.com>
2022-07-04 17:39:13 +0200

    fix: working on edge cases

--- all_inclusive.ts
+++ all_inclusive.ts
@@ -1,8 +1,12 @@
 export function containAllRots(str: string, arr: string[]): boolean {
-  let result: boolean = True;
+  let result: boolean = true;
+  if(str === ""){
+    return result;
+  }
   for(let i: number = 0; i < arr.length; i++){
+    console.log(str)
     if(!arr.includes(str)){
-      return False;
+      return false;
     }
     str = str.substring(1) + str.substring(0,1);
   }
