805b3564ededb5ffd200aba583c6c062e23bb52c
gmahone <gmahone@gmail.com>
2022-07-06 14:19:10 +0200

    fix: initial solution arguments to array problems

--- predict_your_age.ts
+++ predict_your_age.ts
@@ -1,5 +1,5 @@
 export function predictAge(age1:number, age2:number, age3:number,age4:number,age5:number,age6:number,age7:number,age8:number): number{
-  let args: number[] = [...arguments];
-  let result: number = (args.reduce((acc,c) => acc + c**2, 0)**0.5) / 2;
+  let args: number[] = Array.prototype.slice.call(arguments)
+  let result: number = Math.floor((args.reduce((acc,c) => acc + c**2, 0)**0.5) / 2);
   return result;
 };
