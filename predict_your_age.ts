5b85d85b1e7b34751a47969f42dde4abebdba008
gmahone <gmahone@gmail.com>
2022-07-06 13:59:06 +0200

    feat: add initial solution

--- predict_your_age.ts
+++ predict_your_age.ts
@@ -1,3 +1,5 @@
 export function predictAge(age1:number, age2:number, age3:number,age4:number,age5:number,age6:number,age7:number,age8:number): number{
-  return 0;
+  let args: number[] = [...arguments];
+  let result: number = (args.reduce((acc,c) => acc + c**2, 0)**0.5) / 2;
+  return result;
 };
