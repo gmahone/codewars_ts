fb76ddf9e1549e51d408c33be7622ed23deffef8
gmahone <gmahone@gmail.com>
2022-07-06 13:07:59 +0200

    feat: add initial solution

--- strong_number.ts
+++ strong_number.ts
@@ -1,3 +1,12 @@
+export function factorial(n: number): number {
+  if(n > 1){
+    return(n * factorial(n - 1));
+  } else {
+    return 1
+  }           
+}
+
 export const strongNumber = (num: number): string => {
-  return "";
+  let calcStrong: number = String(num).split("").reduce((acc,c) => acc + factorial(+c), 0);
+  return calcStrong === num ? "STRONG!!!!" : "Not Strong !!";
 };
