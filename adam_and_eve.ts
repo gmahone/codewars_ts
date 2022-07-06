6f0fa9f3b8d85f68807b7a169554212fa7f11cfe
Greg Mahone <gmahone@gmail.com>
2022-07-06 22:38:19 +0200

    feat: add simpler solution version

--- adam_and_eve.ts
+++ adam_and_eve.ts
@@ -19,3 +19,20 @@ export class Man extends Human{
 
 export class Woman extends Human{
 }
+
+
+// simple version
+export class Human {}
+
+export class Woman extends Human {}
+
+export class Man extends Human {}
+
+export class God {
+  static create() {
+    return [new Man(), new Woman()];
+  }
+}
+
+
+
