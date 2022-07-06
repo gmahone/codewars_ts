b4f56272e72da86efb4ad39dd3813bcc33d75445
gmahone <gmahone@gmail.com>
2022-07-06 12:32:31 +0200

    feat: add arrow function solution using truthy/falsy

--- nth_power.ts
+++ nth_power.ts
@@ -1,3 +1,7 @@
 export function index(array: number[], n: number): number {
   return array.length < n+1 ? -1 : array[n]**n;
 }
+
+
+// comparing truthy/falsy values
+export const index = (array: number[], n: number): number => array[n] ** n || -1;
