3f0804ce7959742741281f50dfe18ae78d5d3b50
Greg Mahone <gmahone@gmail.com>
2022-07-08 14:45:15 +0200

    fix: use spread to stop mutation of input array

--- sorted_yes_no.ts
+++ sorted_yes_no.ts
@@ -1,7 +1,7 @@
 export function isSortedAndHow(array:number[]): string {
   let result: string = "no"
-  let descendingArray: string = array.sort((a,z) => z - a).join("")
-  let ascendingArray: string = array.sort((a,z) => a - z).join("")
+  let descendingArray: string = [...array].sort((a,z) => z - a).join("")
+  let ascendingArray: string = [...array].sort((a,z) => a - z).join("")
   if(array.join("") === ascendingArray){
     result = "yes, ascending";
   }
