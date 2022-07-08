816517b4626c9c0a7c6a398f08fcd4cf3d0a4420
Greg Mahone <gmahone@gmail.com>
2022-07-08 12:43:23 +0200

    fix: add filter to remove duplicates

--- maximum_triplet_sum.ts
+++ maximum_triplet_sum.ts
@@ -1,3 +1,3 @@
 export function maxTriSum(nums: number[]): number  {
-  return nums.sort((a,z) => z - a).splice(0,3).reduce((acc,c) => acc + c, 0);
+  return nums.filter((e,i,arr) => arr.indexOf(e) === i).sort((a,z) => z - a).splice(0,3).reduce((acc,c) => acc + c, 0);
 }
