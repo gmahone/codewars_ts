ec59ca61e5eff0d7eb5cb51bc8c62f2dbcad65e8
Greg Mahone <gmahone@gmail.com>
2022-07-08 14:42:32 +0200

    fix: need to keep array from mutating

--- sorted_yes_no.ts
+++ sorted_yes_no.ts
@@ -1,9 +1,11 @@
 export function isSortedAndHow(array:number[]): string {
   let result: string = "no"
-  if(array.join("") === array.sort((a,z) => a - z).join("")){
+  let descendingArray: string = array.sort((a,z) => z - a).join("")
+  let ascendingArray: string = array.sort((a,z) => a - z).join("")
+  if(array.join("") === ascendingArray){
     result = "yes, ascending";
   }
-  if(array.join("") === array.sort((a,z) => z - a).join("")){
+  if(array.join("") === descendingArray){
     result = "yes, descending";
   }
   return result;
