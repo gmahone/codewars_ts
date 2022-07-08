bee9cb4f5ae72dd57244d88cff0291d9fa96d44d
Greg Mahone <gmahone@gmail.com>
2022-07-08 14:41:01 +0200

    fix: sorted changed to sort

--- sorted_yes_no.ts
+++ sorted_yes_no.ts
@@ -1,9 +1,9 @@
 export function isSortedAndHow(array:number[]): string {
   let result: string = "no"
-  if(array.join("") === array.sorted((a,z) => a - z).join("")){
+  if(array.join("") === array.sort((a,z) => a - z).join("")){
     result = "yes, ascending";
   }
-  if(array.join("") === array.sorted((a,z) => z - a).join("")){
+  if(array.join("") === array.sort((a,z) => z - a).join("")){
     result = "yes, descending";
   }
   return result;
