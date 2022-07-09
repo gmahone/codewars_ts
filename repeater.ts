a1e231227e2cfac2f58e82be2793fe340cef1dff
Greg Mahone <gmahone@gmail.com>
2022-07-09 13:59:40 +0200

    feat: add solution using repeat

--- repeater.ts
+++ repeater.ts
@@ -2,3 +2,8 @@ export function repeater(str: string, n: number): string {
   var result:string =  new Array(n).fill(str).join("");
   return result;
 }
+
+// solution using repeat
+export function repeater(str: string, n: number): string {
+  return str.repeat(n);
+}
