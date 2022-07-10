ea6764bdd8075e1c6f0e1752723b2d3d687d1ed5
Greg Mahone <gmahone@gmail.com>
2022-07-10 20:48:51 +0200

    fix: charCodeAt requires an index

--- word_values.ts
+++ word_values.ts
@@ -1,7 +1,11 @@
 export function wordCalc(str: string): number {
-  return str.split(" ").join("").reduce((acc,c) => acc + (c.charCodeAt() - 96),0);
+  let result: number = str.split(" ")
+  .join("")
+  .split("")
+  .reduce((acc,c) => acc + (c.charCodeAt(0) - 96),0);
+  return result;
 }
 
 export function wordValue(arr: string[]): number[] {
-  return arr.map(a => wordCalc(a));
+  return arr.map((a,i) => wordCalc(a)*(i+1));
 }
