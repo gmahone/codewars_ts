61c28ae8fc9e67eef5d9663a031fecbd294a5cb7
Greg Mahone <gmahone@gmail.com>
2022-07-10 20:43:01 +0200

    feat: add word calculation function

--- word_values.ts
+++ word_values.ts
@@ -1,3 +1,7 @@
-export function wordValue(arr: string[]) {
-  // your code here
+export function wordCalc(str: string): number {
+  return str.split(" ").join("").reduce((acc,c) => acc + (c.charCodeAt() - 96),0);
+}
+
+export function wordValue(arr: string[]): number[] {
+  return "";
 }
