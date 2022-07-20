b351054ddaf2f5dc78c96c2eb76c78e010545bb6
Greg Mahone <gmahone@gmail.com>
2022-07-20 22:25:16 +0200

    feat: add initial solution

--- rule_of_divisibility_by_7.ts
+++ rule_of_divisibility_by_7.ts
@@ -1,3 +1,12 @@
 export function seven(m: number): number[] {
-  // your code
+  let i: number = 0;
+  let strArray: string[];
+  let strArrayEnd : string[];
+  while(number >= 100){
+    strArray = String(number).split("")
+    strArrayEnd = strArray.pop()
+    number = Number(strArray.join("")) - (2 * Number(strArrayEnd));
+    i++;
+  }
+  return [i, number];
 }
