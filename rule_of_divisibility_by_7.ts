1a6f2ba5d62a79a82d94338d95ef18dabfcb96ac
Greg Mahone <gmahone@gmail.com>
2022-07-20 22:30:47 +0200

    fix: working solution with no function-internal types

--- rule_of_divisibility_by_7.ts
+++ rule_of_divisibility_by_7.ts
@@ -1,12 +1,12 @@
-export function seven(m: number): number[] {
-  let i: number = 0;
-  let strArray: string[];
-  let strArrayEnd : string;
+export function seven(m: number): number[]{
+  let i = 0;
+  let strArray;
+  let strArrayEnd;
   while(m >= 100){
     strArray = String(m).split("");
-    strArrayEnd = strArray.pop()
+    strArrayEnd = strArray.pop();
     m = Number(strArray.join("")) - (2 * Number(strArrayEnd));
     i++;
   }
-  return [i, m];
+  return [m, i];
 }
