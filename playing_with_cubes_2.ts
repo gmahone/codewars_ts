56e5d320043edf20bfdd3d2eecece0e878d5a1bb
Greg Mahone <gmahone@gmail.com>
2022-08-13 21:32:24 +0200

    feat: add placeholder object

--- playing_with_cubes_2.ts
+++ playing_with_cubes_2.ts
@@ -1,1 +1,12 @@
+export class Cube {
+  
+  private _side: number;
 
+  public getSide(): number {
+    return this._side;
+  }
+
+  public setSide(value: number) {
+    this._side = Math.abs(value);
+  }
+}
