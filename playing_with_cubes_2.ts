cc83e9b47f037324524beb76e00479630fbd4ecd
Greg Mahone <gmahone@gmail.com>
2022-08-13 21:41:16 +0200

    feat: add initial solution

--- playing_with_cubes_2.ts
+++ playing_with_cubes_2.ts
@@ -1,12 +1,16 @@
 export class Cube {
   
   private _side: number;
+  
+  constructor(side: number) {
+    this._side = side;
+  }
 
   public getSide(): number {
     return this._side;
   }
 
-  public setSide(value: number) {
+  public setSide(value: number = 0) {
     this._side = Math.abs(value);
   }
 }
