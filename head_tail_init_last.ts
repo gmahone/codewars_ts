f6077efcdb03f4316317b77f31b80075186fbe92
Greg Mahone <gmahone@gmail.com>
2022-09-01 14:14:57 +0200

    feat: add initial solution function tail

--- head_tail_init_last.ts
+++ head_tail_init_last.ts
@@ -2,8 +2,8 @@ export const head =(arr)=>{
   return arr.slice(arr, 0, 1);
 }
 
-export const tail =()=>{
-  throw new Error('Todo, as well as the other functions...')
+export const tail =(arr)=>{
+  return arr.slice(arr, 1);
 }
 
 export const init =()=>{
