cebe58c7d9bd42b76fe458c695b51d886a6c923e
Greg Mahone <gmahone@gmail.com>
2022-09-01 14:20:47 +0200

    feat: add initial solution with types and debug

--- head_tail_init_last.ts
+++ head_tail_init_last.ts
@@ -1,15 +1,15 @@
-export const head =(arr)=>{
-  return arr.slice(0, 1);
+export const head =(arr: number[]): number =>{
+  return arr.slice(0, 1)[0];
 }
 
-export const tail =(arr)=>{
+export const tail =(arr: number[]): number[] =>{
   return arr.slice(1);
 }
 
-export const init =(arr)=>{
+export const init =(arr: number[]): number[] =>{
   return arr.slice(0,arr.length-1); 
 }
 
-export const last =(arr)=>{
-  return arr.slice(arr.length-1);
+export const last =(arr: number[]): number =>{
+  return arr.slice(arr.length-1)[0];
 }
