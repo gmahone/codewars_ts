77c1029892907b2e8a130842022bba85b2ce7a93
Greg Mahone <gmahone@gmail.com>
2022-09-01 14:23:07 +0200

    feat: add cleaned up and simpler initial solution

--- head_tail_init_last.ts
+++ head_tail_init_last.ts
@@ -14,6 +14,12 @@ export const last =(arr: number[]): number =>{
   return arr.slice(arr.length-1)[0];
 }
 
+// cleaned up initial solution
+export const head = (a: number[]) => a[0];
+export const tail = (a: number[]) => a.slice(1);
+export const init = (a: number[]) => a.slice(0, a.length-1);
+export const last = (a: number[]) => a[a.length-1];
+
 
 // another solution with explicit input calling
    export const last = (xs: number[]) => xs[xs.length - 1];
