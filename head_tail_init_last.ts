bdf31e68bf56c7828b5cf0f2de9b71ef65a2e364
Greg Mahone <gmahone@gmail.com>
2022-09-01 14:12:12 +0200

    feat: add placeholder function tail

--- head_tail_init_last.ts
+++ head_tail_init_last.ts
@@ -1,3 +1,7 @@
 export const head =()=>{
   throw new Error('Todo, as well as the other functions...')
 }
+
+export const tail =()=>{
+  throw new Error('Todo, as well as the other functions...')
+}
