55be3da9be3cfa5a681e075dfea91b0863418226
Greg Mahone <gmahone@gmail.com>
2022-09-02 17:20:45 +0200

    feat: add sorting

--- concat_and_join.ts
+++ concat_and_join.ts
@@ -3,4 +3,6 @@ export function bigToSmall(arr: number[][]): string {
   for(let i = 0; i < arr.length, i++){
     result = result.concat(arr[i]);
   }
+  result.sort((a,z) => z - a);
+  console.log(result);
 }
