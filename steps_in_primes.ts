5d37bc2b197d9034cdb675aada7a903b8aa762d2
Greg Mahone <gmahone@gmail.com>
2022-09-06 16:32:01 +0200

    feat: add function to check if prime

--- steps_in_primes.ts
+++ steps_in_primes.ts
@@ -1,3 +1,12 @@
+export function isPrime(p:number): boolean {
+  for(let i: number = 2; i < p; i++){
+    if(p % i == 0){
+      return false;
+    }
+  }
+  return true;
+}
+
 export function step(g:number, m:number, n:number): [number,number]|null {
   return null
 }
