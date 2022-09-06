df33892a1622077b1bf135913aeec5c6d2e8257d
Greg Mahone <gmahone@gmail.com>
2022-09-06 21:57:15 +0200

    feat: add my solution but exceeds time

--- steps_in_primes.ts
+++ steps_in_primes.ts
@@ -58,3 +58,31 @@ export function step(g:number, m:number, n:number): [number,number]|null {
   }
   return null
 }
+
+
+
+// my solution but won't pass based on time
+export function isPrime(p:number): boolean {
+  for(let i: number = 3; i < Math.floor(p/2); i += 2){
+    if(p % i == 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+export function step(g:number, m:number, n:number): [number,number]|null {
+  let primeMap = new Map<number, number>();
+  if(m % 2 == 0){
+    m++;
+  }
+  for(let i: number = m; i <= n; i += 2){
+    if(isPrime(i)){
+      primeMap.set(i, i - g);
+      if(primeMap.has(i - g)){
+        return([i - g, i]);
+      }
+    }
+  }
+  return null
+}
