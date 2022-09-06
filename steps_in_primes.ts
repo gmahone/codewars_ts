97b72b890f9c457f2d7036dc78ad7eeb7f983305
Greg Mahone <gmahone@gmail.com>
2022-09-06 21:40:16 +0200

    feat: add array version but its slower

--- steps_in_primes.ts
+++ steps_in_primes.ts
@@ -26,3 +26,35 @@ export function step(g:number, m:number, n:number): [number,number]|null {
   return null
 }
 
+
+
+
+// trying an array version
+// this is slower than a map version above
+export function isPrime(p:number): boolean {
+  for(let i: number = 3; i < Math.floor(p/2); i += 2){
+    if(p % i == 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+export function step(g:number, m:number, n:number): [number,number]|null {
+  let primeArray: number[] = [];
+  if(m % 2 == 0){
+    m++;
+  }
+  if(n % 2 == 0){
+    n--;
+  }
+  for(let i: number = m; i <= n; i += 2){
+    if(isPrime(i)){
+      primeArray.push(i);
+      if(primeArray.includes(i - g)){
+        return([i - g, i]);
+      }
+    }
+  }
+  return null
+}
