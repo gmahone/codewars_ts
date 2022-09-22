33b782fc9874c97ad1ca0473122574818932772c
Greg Mahone <gmahone@gmail.com>
2022-09-22 21:45:09 +0200

    feat: add initial solution

--- calculate_mean_and_concatenate_string.ts
+++ calculate_mean_and_concatenate_string.ts
@@ -1,3 +1,5 @@
 export function mean(lst: string[]): [number, string] {
-  return "";
+  let listMean: number = lst.filter(e => e == Number(e)).reduce((acc, c) => acc + Number(c), 0)/10
+  let listConcat: string = lst.filter(e => e != Number(e)).join("")
+  return [listMean, listConcat];
 }
