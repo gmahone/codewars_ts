95c318fb89877946aff6ec075737b1e1ce4abe89
Greg Mahone <gmahone@gmail.com>
2022-09-23 20:45:56 +0200

    fix: working on string filtering

--- calculate_mean_and_concatenate_string.ts
+++ calculate_mean_and_concatenate_string.ts
@@ -1,9 +1,12 @@
+export function notNumber(x: string): boolean {
+  return !(Number(x))
+}
+
 export function mean(lst: string[]): [number, string] {
   let listNum: number[] = lst.filter(Number).map(a => +a);
   let listMean: number = listNum.reduce((acc,c) => acc + Number(c)) / 10;
   console.log(listMean);
-  let listString: string[] = lst.filter(!Number);
-  let listConcat: string = listString.join("");
+  let listString: string[] = lst.filter(notNumber);
   console.log(listConcat);
   return [10, "A"];
 }
