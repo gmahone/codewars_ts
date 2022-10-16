12dd9c2dee158138df0893f785e4e45197729479
Greg Mahone <gmahone@gmail.com>
2022-10-16 21:24:53 +0200

    fix: update sorting function

--- sort_arrays.ts
+++ sort_arrays.ts
@@ -1,4 +1,12 @@
 export function sortMe (names: string[]): string[] {
-  const result: string[] = names.sort((a,z) => a - z);
+  const result: string[] = names.sort((n1,n2) => {
+    if (n1 > n2) {
+        return 1;
+    }
+    if (n1 < n2) {
+        return -1;
+    }
+    return 0;
+  });
   return(result);
 }
